540a2485b641d451884761515c17fc63eecb42a3
sanidhya <sanidhya09@gmail.com>
2023-01-16 20:01:06 +0530

    Updated Local Notification

--- app/src/main/java/com/example/pushnotification/MainActivity.kt
+++ app/src/main/java/com/example/pushnotification/MainActivity.kt
@@ -1,12 +1,15 @@
-
 package com.example.pushnotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +20,12 @@ class MainActivity : AppCompatActivity() {
             getString(R.string.channel_id),
             getString(R.string.channel_name)
         )
+        createNotification()
     }
+
+
+
+
     private fun createChannel(channelId: String, channelName: String) {
         // TODO: Step 1.6 START create a channel
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -44,6 +52,30 @@ class MainActivity : AppCompatActivity() {
 
             notificationManager.createNotificationChannel(notificationChannel)
 
+        }
+    }
+
+    private fun createNotification() {
+
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, SecondActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(this, getString(R.string.channel_id))
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(0, builder.build())
         }
     }
 
